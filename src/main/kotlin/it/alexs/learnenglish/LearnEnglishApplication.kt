9bd93f4c36b2da0f2dab25a14a746e3e1beddc78
Alessandro <alex.spasiano@gmail.com>
2024-12-27 14:05:58 +0100

    initial commit

---
+++ src/main/kotlin/it/alexs/learnenglish/LearnEnglishApplication.kt
@@ -0,0 +1,11 @@
+package it.alexs.learnenglish
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearnEnglishApplication
+
+fun main(args: Array<String>) {
+    runApplication<LearnEnglishApplication>(*args)
+}
---
+++ src/test/kotlin/it/alexs/learnenglish/LearnEnglishApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.alexs.learnenglish
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearnEnglishApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
